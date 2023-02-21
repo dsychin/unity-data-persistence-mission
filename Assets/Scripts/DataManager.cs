ea5d6c0e682b71bf3654be3195d5a799e79f356a
Desmond Chin <chin.sy.des@gmail.com>
2023-02-22 08:41:32 +0900

    implement data persistence between sessions

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,12 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
     public static DataManager Instance;
     public static string PlayerName;
-    public static ScoreRecord HighScore = new ScoreRecord{};
+    public static ScoreRecord HighScore = new ScoreRecord { };
 
     private void Awake()
     {
@@ -15,6 +17,7 @@ public class DataManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
+        LoadHighScore();
         Instance = this;
         DontDestroyOnLoad(Instance);
     }
@@ -26,11 +29,39 @@ public class DataManager : MonoBehaviour
             PlayerName = PlayerName,
             Points = points
         };
+        SaveHighScore();
     }
 
+    [Serializable]
     public class ScoreRecord
     {
         public string PlayerName;
         public int Points;
+    }
+
+    public class SaveData
+    {
+        public ScoreRecord HighScore;
+    }
+
+    static void SaveHighScore()
+    {
+        var saveData = new SaveData
+        {
+            HighScore = HighScore
+        };
+        var json = JsonUtility.ToJson(saveData);
+        File.WriteAllText(Application.persistentDataPath + "/saveData.json", json);
+    }
+
+    static void LoadHighScore()
+    {
+        if (!File.Exists(Application.persistentDataPath + "/saveData.json"))
+        {
+            return;
+        }
+        var json = File.ReadAllText(Application.persistentDataPath + "/saveData.json");
+        var saveData = JsonUtility.FromJson<SaveData>(json);
+        HighScore = saveData.HighScore;
     }
 }
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -13,20 +13,22 @@ public class MainManager : MonoBehaviour
     public Text ScoreText;
     public Text HighScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        SetHighScoreText();
+
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -74,9 +76,15 @@ public class MainManager : MonoBehaviour
         if (m_Points > DataManager.HighScore.Points)
         {
             DataManager.UpdateHighScore(m_Points);
-            HighScoreText.text = $"Best Score : {DataManager.HighScore.PlayerName} : {DataManager.HighScore.Points.ToString()}";
+            SetHighScoreText();
         }
         m_GameOver = true;
         GameOverText.SetActive(true);
+    }
+
+    void SetHighScoreText()
+    {
+        var playerName = DataManager.HighScore.Points == 0 ? DataManager.PlayerName : DataManager.HighScore.PlayerName;
+        HighScoreText.text = $"Best Score : {playerName} : {DataManager.HighScore.Points.ToString()}";
     }
 }
